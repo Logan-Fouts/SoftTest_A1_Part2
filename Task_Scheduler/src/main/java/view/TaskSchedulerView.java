e85985cc9033bde0995def9381149d8a13c5178b
lf222yv <lf222yv@student.lnu.se>
2023-11-20 13:21:49 -0500

    GREEN

--- Task_Scheduler/src/main/java/view/TaskSchedulerView.java
+++ Task_Scheduler/src/main/java/view/TaskSchedulerView.java
@@ -1,5 +1,20 @@
 package view;
 
-public class TaskSchedulerView {
+import model.Task;
 
+public class TaskSchedulerView {
+    private IInput input;
+    private IOutput output;
+
+    public TaskSchedulerView(IInput input, IOutput output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void showMenu() {
+        output.println("1. Add Task");
+        output.println("2. View Tasks");
+        output.println("3. Remove All Tasks Named ...");
+        output.println("0. Exit");
+    }
 }
