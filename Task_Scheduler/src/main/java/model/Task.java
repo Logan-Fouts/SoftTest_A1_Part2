f7aef6c957eae02ff9619029f50e54950d835bf4
lf222yv <lf222yv@student.lnu.se>
2023-11-20 10:44:08 -0500

    REFACTOR

--- Task_Scheduler/src/main/java/model/Task.java
+++ Task_Scheduler/src/main/java/model/Task.java
@@ -17,14 +17,18 @@ public class Task {
 
   public Task(String givenTitle, String givenDescription, String givenAlertDate) {
     this(givenTitle);
-    this.title = givenTitle;
     this.description = givenDescription;
     this.alertDate = LocalDate.parse(givenAlertDate);
   }
 
-  // The alert date given as a string in "YYYY-MM-DD" format.
+  /**
+   * Sets Alert Date for Task.
+   * 
+   * @param givenAlertDate The alert date given as a string in "YYYY-MM-DD"
+   *                       format.
+   */
   public void setAlertDate(String givenAlertDate) {
-    this.alertDate = LocalDate.parse(givenAlertDate); 
+    this.alertDate = LocalDate.parse(givenAlertDate);
   }
 
   public void setDescription(String givenDescription) {
