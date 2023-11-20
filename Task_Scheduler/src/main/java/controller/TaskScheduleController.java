3491de7c51336d6ef69332f5ac9191898de56707
lf222yv <lf222yv@student.lnu.se>
2023-11-20 14:08:45 -0500

    GREEN

--- Task_Scheduler/src/main/java/controller/TaskScheduleController.java
+++ Task_Scheduler/src/main/java/controller/TaskScheduleController.java
@@ -1,5 +1,26 @@
 package controller;
 
+import model.TaskManager;
+import view.TaskSchedulerView;
+
 public class TaskScheduleController {
-  
+  private TaskManager taskManager;
+  private TaskSchedulerView taskView;
+
+  public TaskScheduleController(TaskManager taskManager, TaskSchedulerView taskView) {
+    this.taskManager = taskManager;
+    this.taskView = taskView;
+  }
+
+  public void start() {
+    boolean running = true;
+    while (running) {
+      int choice = taskView.getUserMenuSelection();
+      switch (choice) {
+        case 0:
+          running = false;
+          break;
+      }
+    }
+  }
 }
