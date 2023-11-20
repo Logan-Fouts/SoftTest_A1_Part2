22c70e72e12b10c1d303139985dee1bfb6ad0460
lf222yv <lf222yv@student.lnu.se>
2023-11-20 09:40:47 -0500

    RED

--- Task_Scheduler/src/test/java/TaskTest.java
+++ Task_Scheduler/src/test/java/TaskTest.java
@@ -10,7 +10,15 @@ public class TaskTest {
   @Test
   public void shouldNotReturnNullTitleWhenGetTask() {
     Task task = new Task("Complete A1 Part 2");
+
     assertNotNull("Task title should not be null", task.getTitle());
+  }
+
+  @Test
+  public void shouldNotReturnNullDateWhenGetDate() {
+    Task task = new Task("Do School Work");
+
+    assertNotNull("Task alertDate should not be null", task.getAlertDate());
   }
 
 }
