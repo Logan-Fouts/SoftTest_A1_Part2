5a283545ed931d81cdf77453fbfcaed97e3924ab
lf222yv <lf222yv@student.lnu.se>
2023-11-20 13:33:45 -0500

    RED

--- Task_Scheduler/src/test/java/TaskSchedulerViewTest.java
+++ Task_Scheduler/src/test/java/TaskSchedulerViewTest.java
@@ -1,8 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import view.IInput;
 import view.IOutput;
@@ -24,5 +26,20 @@ public class TaskSchedulerViewTest {
     verify(mockOutput, times(1)).println("2. View Tasks");
     verify(mockOutput, times(1)).println("3. Remove All Tasks Named ...");
     verify(mockOutput, times(1)).println("0. Exit");
+  }
+
+  @Test
+  public void shouldReturnOneWhenEnteredInMenue() {
+    IInput mockInput;
+    IOutput mockOutput;
+    TaskSchedulerView taskView;
+
+    mockInput = mock(IInput.class);
+    mockOutput = mock(IOutput.class);
+    taskView = new TaskSchedulerView(mockInput, mockOutput);
+
+    Mockito.when(mockInput.readInt()).thenReturn(1);
+
+    assertEquals(1, taskView.getUserMenuSelection());
   }
 }
