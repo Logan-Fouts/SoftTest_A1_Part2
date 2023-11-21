95addf7e6114ea153a4d9925493d6eb5072ecce6
lf222yv <lf222yv@student.lnu.se>
2023-11-21 11:19:29 -0500

    GREEN

--- Task_Scheduler/src/main/java/controller/TaskScheduleController.java
+++ Task_Scheduler/src/main/java/controller/TaskScheduleController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Task;
 import model.TaskManager;
 import view.TaskSchedulerView;
 
@@ -18,6 +19,10 @@ public class TaskScheduleController {
       int choice = taskView.getUserMenuSelection();
       switch (choice) {
         case 0:
+          running = false;
+          break;
+        case 1:
+          Task taskToAdd = taskView.getTaskDetails();
           running = false;
           break;
         default:
